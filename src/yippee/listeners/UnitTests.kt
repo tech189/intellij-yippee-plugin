4990c6a2ad956d17c409a37dba2c54e968a8f292
tech189 <dlloyd189@gmail.com>
2022-12-25 10:51:22 +0000

    Add yippee notification!!!

--- src/yippee/listeners/UnitTests.kt
+++ src/yippee/listeners/UnitTests.kt
@@ -6,6 +6,7 @@ import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
+import yippee.util.*
 
 class UnitTests(private val project: Project, private val listener: Listener): Restartable {
     override fun start(disposable: Disposable) {
@@ -14,7 +15,10 @@ class UnitTests(private val project: Project, private val listener: Listener): R
                 if (notification.groupId == TestsUIUtil.NOTIFICATION_GROUP.displayId) {
                     val testsFailed = notification.type == NotificationType.ERROR
                     if (testsFailed) listener.onUnitTestFailed()
-                    else listener.onUnitTestSucceeded()
+                    else {
+                      listener.onUnitTestSucceeded()
+                      show("Yippee!!! <img src=\"https://c.tenor.com/mJU99ZjtlQsAAAAM/tbh-creature.gif\"/>")
+                    }
                 }
             }
         })
